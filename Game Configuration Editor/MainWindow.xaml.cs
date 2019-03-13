6f57f40c063c4df50f0d3276bb41cdb120c47a5b
Fletcher R A (FCES) <15056252@students.southwales.ac.uk>
2019-03-13 19:19:35 +0000

    Updated Games List
    Added buttons to the Games List. Buttons contain pathing locations to each
    of their respective config files.

--- Game Configuration Editor/MainWindow.xaml.cs
+++ Game Configuration Editor/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using System.Windows.Forms;
 using System.IO;
 using System.Collections.Generic;
 
-
 namespace Game_Configuration_Editor
 {
     /// <summary>
@@ -12,15 +11,15 @@ namespace Game_Configuration_Editor
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Used to read the file location for scanning and identifying supported config files
+        List<string> read = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
             ConfigList();
         }
-
-        // Used to read the file location for scanning and identifying supported config files
-        List<string> read = new List<string>();
-            
+          
         // Writes each of the configuration files into a rich text box
         private void ConfigList()
         {
@@ -57,35 +56,52 @@ namespace Game_Configuration_Editor
             }
         }
 
+        private void btnGameFileClick(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Button btn = (System.Windows.Controls.Button)sender;
+
+            string fileURL = btn.Content.ToString();
+
+            ///////// Continue from HERE /////////
+            // Load file here
+
+            OpenFileDialog openFile = new OpenFileDialog();
+
+        }
+
         //  Allows the program to scan the current directory for the file types designated
         private void btnScan_Click(object sender, RoutedEventArgs e)
         {
-
+            // Used to bring up error message safely
             try
             {
+                // Clears the scanned list to prevent repeating buttons
                 spGameList.Children.Clear();
+
+                // Scans for the amount of files inside of ConfigFiles.txt
                 for (int i = 0; i < read.Count; i++)
                 {
-                    //string[] temp = Directory.GetFiles(txtDestination.Text, read[i], SearchOption.AllDirectories);
+                    // String array that designates which folder to search in, what to search for, and which directories to search inside of
                     string[] found = Directory.GetFiles(txtDestination.Text, read[i], SearchOption.AllDirectories);
 
+                    // Creates new button 'btnGameFile'
                     System.Windows.Controls.Button btnGameFile = new System.Windows.Controls.Button();
 
-                    //filePaths.Add(temp);
-
-                    // Inputs the scanned documents to the list box
-                    //rtbGameConfigFiles.AppendText(read[i] + "\r");
-
-
+                    // Inputs the file path for each config file found into the content of each button
                     btnGameFile.Content = found[0];
+                    // Inputs the file name for each button to differentiate them
                     btnGameFile.Name = "btn" + i.ToString();
-
+                    // Creates new event for each button that allows each button to be programmed in a separate function
+                    btnGameFile.Click += new RoutedEventHandler(btnGameFileClick);
+                    // Dynamically creates a new button
                     spGameList.Children.Add(btnGameFile);
                 }
             }
+            // If an issue finding the directory occurs, catch the issue to display an error
             catch (Exception r)
             {
                 Console.WriteLine("Exception: " + r.Message);
+                // Displays an error to a message box
                 System.Windows.Forms.MessageBox.Show("Please select a directory", "ERROR: No Directory", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
